3d83c842d7a3b82f676b1cbd01729a2f16962763
githubmor <pc2man@gmail.com>
2025-05-19 14:29:05 +0330

    add link to pages

--- app/components/header/navs/show-drop/index.tsx
+++ app/components/header/navs/show-drop/index.tsx
@@ -1,30 +1,31 @@
 "use client";
 import { useState } from "react";
 import DownIcon from "./down-icon";
+import Link from 'next/link';
 
 function ShopDrop() {
   const [showList, setShowList] = useState(false);
   return (
     <div className="flex flex-col gap-2 relative">
-      <div
-        role="button"
-        className="flex-ic gap-1 "
-        onClick={() => setShowList((c) => !c)}
-      >
+      <div role="button" className="flex-ic gap-1 " onClick={() => setShowList((c) => !c)}>
         <span>Shop</span>
-        <DownIcon className={`${showList ? "rotate-180" : ""} duration-100`} />
+        <DownIcon className={`${showList ? 'rotate-180' : ''} duration-100`} />
       </div>
-      <div
+      <ul
         className={`w-48 flex flex-col gap-2 ps-2 transition-all duration-500 overflow-hidden  ${
-          showList
-            ? "max-h-48 h-full lg:h-auto lg:p-4   "
-            : "max-h-0 h-0 -mt-2 p-0 !border-0 !rounded-none"
+          showList ? 'max-h-48 h-full lg:h-auto lg:p-4   ' : 'max-h-0 h-0 -mt-2 p-0 !border-0 !rounded-none'
         } lg:absolute left-0 top-full lg:mt-2 lg:border  lg:shadow-sm lg:rounded-xl z-10 bg-base `}
       >
-        <span className="text-sm">Men</span>
-        <span className="text-sm">Women</span>
-        <span className="text-sm">Causal</span>
-      </div>
+        <Link href="/category/Men" onClick={() => setShowList(false)} className="text-sm">
+          Men
+        </Link>
+        <Link href="/category/Women" onClick={() => setShowList(false)} className="text-sm">
+          Women
+        </Link>
+        <Link href="/category/Causal" onClick={() => setShowList(false)} className="text-sm">
+          Causal
+        </Link>
+      </ul>
     </div>
   );
 }
--- app/components/header/title/index.tsx
+++ app/components/header/title/index.tsx
@@ -1,12 +1,13 @@
-import Hamburger from "./hamburger";
+import Link from 'next/link';
+import Hamburger from './hamburger';
 
 function Title({ onToggle }: { onToggle: () => void }) {
   return (
     <div className="flex-ic gap-4 row-start-1 col-start-1 ">
       <Hamburger onClick={onToggle} />
-      <span className="text-2xl lg:text-[2rem] text-neutral font-anton">
+      <Link href="/" className="text-2xl lg:text-[2rem] text-neutral font-anton">
         SHOP.DR
-      </span>
+      </Link>
     </div>
   );
 }
