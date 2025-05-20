37a9c0d39ebc10eb410849674e0eea0104a9642c
githubmor <pc2man@gmail.com>
2025-05-20 11:24:21 +0330

    fix: remove unnecessary line breaks in AnimatNumber component

--- app/components/hero/numbers/animat-number.tsx
+++ app/components/hero/numbers/animat-number.tsx
@@ -13,9 +13,7 @@ function AnimatNumber({ num, caption, className = "" }: Props) {
   const formattedNumber = new Intl.NumberFormat("en-US").format(number);
   return (
     <div className={`flex flex-col ${className}`}>
-      <span className="font-bold text-2xl lg:text-[2.5rem] ">
-        {formattedNumber}+
-      </span>
+      <span className="font-bold text-2xl lg:text-[2.5rem] ">{formattedNumber}+</span>
       <span className="text-xs !text-neutral-600  lg:text-base">{caption}</span>
     </div>
   );
