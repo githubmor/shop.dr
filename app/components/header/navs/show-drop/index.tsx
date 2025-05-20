83b3fcb8e45fd92de77df0ad71327a5e0fefa52d
githubmor <pc2man@gmail.com>
2025-05-20 10:46:26 +0330

    feat: replace hardcoded category links with dynamic fetching from API

--- app/components/header/navs/show-drop/index.tsx
+++ app/components/header/navs/show-drop/index.tsx
@@ -2,9 +2,13 @@
 import { useState } from "react";
 import DownIcon from "./down-icon";
 import Link from 'next/link';
+import useSWR from 'swr';
+import { fetcher } from '@/app/utils';
+import { CategoryType } from '@/app/models';
 
 function ShopDrop() {
   const [showList, setShowList] = useState(false);
+  const { data: categories } = useSWR<CategoryType[]>(`/api/category/list`, fetcher);
   return (
     <div className="flex flex-col gap-2 relative">
       <div role="button" className="flex-ic gap-1 " onClick={() => setShowList((c) => !c)}>
@@ -16,15 +20,16 @@ function ShopDrop() {
           showList ? 'max-h-48 h-full lg:h-auto lg:p-4   ' : 'max-h-0 h-0 -mt-2 p-0 !border-0 !rounded-none'
         } lg:absolute left-0 top-full lg:mt-2 lg:border  lg:shadow-sm lg:rounded-xl z-10 bg-base `}
       >
-        <Link href="/category/Men" onClick={() => setShowList(false)} className="text-sm">
-          Men
-        </Link>
-        <Link href="/category/Women" onClick={() => setShowList(false)} className="text-sm">
-          Women
-        </Link>
-        <Link href="/category/Causal" onClick={() => setShowList(false)} className="text-sm">
-          Causal
-        </Link>
+        {categories?.map((category) => (
+          <Link
+            key={category.id}
+            href={`/category/${category.id}`}
+            onClick={() => setShowList(false)}
+            className="text-sm"
+          >
+            {category.title}
+          </Link>
+        ))}
       </ul>
     </div>
   );
