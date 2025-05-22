796498daaa3e2e690038af6551d63b925233d4ce
githubmor <pc2man@gmail.com>
2025-05-22 08:32:40 +0330

    refactor: clean up Footer component layout and improve code readability

--- app/components/footer/index.tsx
+++ app/components/footer/index.tsx
@@ -8,16 +8,13 @@ import Twiter from "./twiter";
 
 function Footer() {
   return (
-    <div className="w-full flex flex-col divide-y divide-neutral-100 gap-6 bg-base-200 px-6 lg:px-24 pt-[1.875rem] pb-20">
+    <div className="w-full flex flex-col gap-6 bg-base-200 px-6 lg:px-24 pt-[1.875rem] pb-20">
       <div className=" flex flex-col lg:flex lg:flex-row gap-4 lg:gap-10 lg:justify-between ">
         <div className="flex flex-col gap-6 lg:gap-9 lg:w-1/4 ">
           <div className="flex flex-col gap-3 lg:gap-6 ">
-            <span className="text-[1.8rem] lg:text-[2.1rem] font-anton">
-              SHOP.DR
-            </span>
+            <span className="text-[1.8rem] lg:text-[2.1rem] font-anton">SHOP.DR</span>
             <span className="text-sm text-neutral-600">
-              We have clothes that suits your style and which you’re proud to
-              wear. From women to men.
+              We have clothes that suits your style and which you’re proud to wear. From women to men.
             </span>
           </div>
           <div className="flex gap-3 items-center ">
@@ -28,38 +25,21 @@ function Footer() {
           </div>
         </div>
         <div className="grid grid-cols-2 gap-3 lg:gap-12 justify-between lg:grid lg:grid-cols-4  ">
-          <LinkColumn
-            title="COMPANY"
-            links={["About", "Features", "Works", "Career"]}
-          />
+          <LinkColumn title="COMPANY" links={['About', 'Features', 'Works', 'Career']} />
           <LinkColumn
             title="HELP"
-            links={[
-              "Customer Support",
-              "Delivery Details",
-              "Terms & Conditions",
-              "Privacy Policy",
-            ]}
+            links={['Customer Support', 'Delivery Details', 'Terms & Conditions', 'Privacy Policy']}
           />
-          <LinkColumn
-            title="FAQ"
-            links={["Account", "Manage Deliveries", "Orders", "Payment"]}
-          />
+          <LinkColumn title="FAQ" links={['Account', 'Manage Deliveries', 'Orders', 'Payment']} />
           <LinkColumn
             title="RESOURCES"
-            links={[
-              "Free eBook",
-              "Development Tutorial",
-              "How to - Blog",
-              "Youtube Playlist",
-            ]}
+            links={['Free eBook', 'Development Tutorial', 'How to - Blog', 'Youtube Playlist']}
           />
         </div>
       </div>
+      <hr />
       <div className="w-full flex flex-col lg:flex lg:flex-row lg:justify-between gap-[1.125rem] items-center  pt-4">
-        <span className=" text-sm text-neutral-600">
-          Shop.Dr © 2020-2025, All Rights Reserved
-        </span>
+        <span className=" text-sm text-neutral-600">Shop.Dr © 2020-2025, All Rights Reserved</span>
         <div className="w-2/3 md:w-1/3 lg:w-1/3 flex gap-3">
           <Badge>
             <Image src="/visa.svg" fill alt="visa" />
