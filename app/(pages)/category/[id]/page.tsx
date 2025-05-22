8b73704ba8fef003aca069d6c47926d38bfb6f6e
githubmor <pc2man@gmail.com>
2025-05-22 08:35:09 +0330

    feat: enhance Category and Style pages with FilterPanel and improved layout

--- app/(pages)/category/[id]/page.tsx
+++ app/(pages)/category/[id]/page.tsx
@@ -1,5 +1,9 @@
 import { getCategory } from '@/app/orm';
 import { Breadcrumbs } from '../../components';
+import { categories, styles, products } from '@/app/db';
+import List from '../../components/list';
+import Menu from '../../components/menu';
+import FilterPanel from '../../shop/components/filter-panel';
 
 type Props = {
   params: Promise<{
@@ -11,9 +15,16 @@ export default async function CategoryPage({ params }: Props) {
   const { id } = await params;
   const category = await getCategory(+id);
   return (
-    <div>
+    <div className="flex flex-col gap-5 ">
       <Breadcrumbs paths={[{ href: `/category/${category?.id}`, label: category?.title || '' }]} />
-      {category?.title}
+      <div className="flex gap-5 ">
+        <FilterPanel>
+          <Menu title="Categories" items={categories} />
+          <hr />
+          <Menu title="Styles" items={styles} />
+        </FilterPanel>
+        <List products={products} title={category?.title || ''} allProductCount={100} page={2} count={10} />
+      </div>
     </div>
   );
 }
--- app/(pages)/style/[id]/page.tsx
+++ app/(pages)/style/[id]/page.tsx
@@ -1,5 +1,9 @@
 import { getStyle } from '@/app/orm';
 import { Breadcrumbs } from '../../components';
+import { categories, styles, products } from '@/app/db';
+import List from '../../components/list';
+import Menu from '../../components/menu';
+import FilterPanel from '../../shop/components/filter-panel';
 
 type Props = {
   params: Promise<{
@@ -11,9 +15,16 @@ async function StylePage({ params }: Props) {
   const { id } = await params;
   const style = await getStyle(+id);
   return (
-    <div>
+    <div className="flex flex-col gap-5 ">
       <Breadcrumbs paths={[{ href: `/style/${style?.id}`, label: style?.title || '' }]} />
-      {style?.title}
+      <div className="flex gap-5 ">
+        <FilterPanel>
+          <Menu title="Categories" items={categories} />
+          <hr />
+          <Menu title="Styles" items={styles} />
+        </FilterPanel>
+        <List products={products} title={style?.title || ''} allProductCount={100} page={2} count={10} />
+      </div>
     </div>
   );
 }
