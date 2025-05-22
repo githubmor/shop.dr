457c99cf8ba0d576f3470414f393a083b63bad62
githubmor <pc2man@gmail.com>
2025-05-22 08:14:13 +0330

    refactor: remove FilterIcon component and integrate FilterPanel in ShopPage

--- app/(pages)/shop/page.tsx
+++ app/(pages)/shop/page.tsx
@@ -1,30 +1,23 @@
-import { getCategories, getStyles } from '@/app/orm';
+import { fetchProductList, getCategories, getStyles } from '@/app/orm';
 import { Breadcrumbs } from '../components';
+import List from '../components/list';
 import Menu from '../components/menu';
-import FilterIcon from './filter-icon';
+import FilterPanel from './components/filter-panel';
 
 async function ShopPage() {
   const categories = await getCategories();
   const styles = await getStyles();
+  const products = await fetchProductList('list');
   return (
     <div className="flex flex-col gap-5 ">
       <Breadcrumbs paths={[{ href: '/shop', label: 'Shop' }]} />
       <div className="flex gap-5 ">
-        <div className="w-1/4 flex flex-col gap-5 rounded-2xl border border-neutral-100 p-5 shadow-sm">
-          <div className="flex items-center justify-between">
-            <h2 className="text-lg font-semibold">Filters</h2>
-            <FilterIcon />
-          </div>
-          <hr />
+        <FilterPanel>
           <Menu title="Categories" items={categories} />
           <hr />
           <Menu title="Styles" items={styles} />
-        </div>
-        <div className="tes w-3/4 ">
-          <div>
-            <span className="text-2xl font-bold">Shop Now</span>
-          </div>
-        </div>
+        </FilterPanel>
+        <List products={products} title={'Shop Now'} allProductCount={100} page={2} count={10} />
       </div>
     </div>
   );
